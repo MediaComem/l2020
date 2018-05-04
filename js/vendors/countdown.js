ac59901b0e8f9a16f49036a26484592306089742
Mathieu Carrard <carrardm@gmail.com>
2018-05-04 15:39:50 +0200

    Update countdown.js

--- js/vendors/countdown.js
+++ js/vendors/countdown.js
@@ -189,7 +189,7 @@
 	                },
 	                success: function() {
 	                    // target = new Date(data.targetDate).getTime();
-	                    target = new Date(1578686400).getTime();
+	                    target = new Date(1578600000).getTime();
 						update_remaining();
 	                    setInterval(update_remaining, 1000);
 	                }
